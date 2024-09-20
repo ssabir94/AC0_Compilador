2e02ddb88cc6c36c99a3b721002e675e582ebfd7
unknown <you@example.com>
2024-09-20 13:43:57 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number1, number2, total;
+
+
+        Console.WriteLine("Dona'm el primer valor: ");
+        number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Dona'm el segon valor: ");
+        number2 = Convert.ToInt32(Console.ReadLine());
+        total = number1 + number2;
+        Console.WriteLine("la suma correcta és: " + total);
+        Console.ReadLine();
+    }
+}
